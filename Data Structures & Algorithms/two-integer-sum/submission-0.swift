425d5c1780ef9413a11039a3a1ddff3927fd42f3
PrayagSrinivas <78862797+PrayagSrinivas@users.noreply.github.com>
2026-07-21 21:26:14 +0530

    Update: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.swift
+++ Data Structures & Algorithms/two-integer-sum/submission-0.swift
@@ -1,17 +1,16 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var hashMap = [Int : Int]()
+        var hashMap = [Int : Int]() // It will store the entry and it's index
 
         for index in 0..<nums.count {
-            let difference = target - nums[index]
-
-            if let value = hashMap[difference] {
-                return [value, index]
+            let complementValue = target - nums[index]
+            if let item = hashMap[complementValue] {
+                return [item, index]
             } else {
                 hashMap[nums[index]] = index
             }
         }
 
-        return []
+        return [-1, -1]
     }
 }
